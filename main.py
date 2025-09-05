a7866d5e066fcceaa8bd6f68acefe98347802207
ankita93anki <ankitasingh7639369@gmail.com>
2025-09-06 00:59:54 +0530

    using pydantic models inside a function

--- main.py
+++ main.py
@@ -6,8 +6,20 @@ class Profile(BaseModel):
     email: str
     age: int
 
+class Product(BaseModel):
+    name:str
+    price:int
+    discount:int 
+    discounted_price:float
+
 app = FastAPI()
+
+@app.post('/addproduct')
+def addProduct(product:Product):
+    product.discounted_price = product.price - (product.price * product.discount)/100
+    return product
 
 @app.post('/adduser')
 def addUser(profile:Profile):
     return profile
+
