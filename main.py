b98827349a3c0ef790a7806394487b8c3918898c
ankita93anki <ankitasingh7639369@gmail.com>
2025-09-05 23:15:08 +0530

    path parameter

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ app = FastAPI()
 def index():
     return 'Hello world!'
 
-@app.get('/property')
-def property():
-    return 'This is a property range'
+#id here is a path parameter
+@app.get('/property/{id}')
+def property(id:int):
+    return {f'This is a property range for property {id}'}
+
+@app.get('/profile/{username}')
+def profile(username: str):
+    return {f'This is profile page for user : {username}'}
 
 @app.get('/movies')
 def movies():
