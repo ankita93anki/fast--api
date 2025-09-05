df04a5511198587f761a361237e8aba4e0c0ad72
ankita93anki <ankitasingh7639369@gmail.com>
2025-09-06 00:35:10 +0530

    pydantic models with request body

--- main.py
+++ main.py
@@ -1,23 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Profile(BaseModel):
+    name: str
+    email: str
+    age: int
 
 app = FastAPI()
 
-@app.get('/user/admin')
-def admin():
-    return {'This is admin page'}
-
-#path parameter
-@app.get('/user/{username}')
-def profile(username):
-    return {f'This is a profile page for {username}'}
-
-#required query parameter
-@app.get('/products')
-def products(id:int=None,price:int=None):
-    return {f'Product with an id: {id} and price = {price}'}
-
-#use path & query parametr
-@app.get('/profile/{userid}/comments')
-def profile(userid:int,commentId:int):
-    return {f'Profile page for user with userid {userid} and comment for comment id {commentId}'}
-
+@app.post('/adduser')
+def addUser(profile:Profile):
+    return profile
