4647a6936fb2c9c927dfe97a44a5aa3e1a5b2cc6
ankita93anki <ankitasingh7639369@gmail.com>
2025-09-05 23:39:12 +0530

    query parameters

--- main.py
+++ main.py
@@ -6,8 +6,14 @@ app = FastAPI()
 def admin():
     return {'This is admin page'}
 
+#path parameter
 @app.get('/user/{username}')
 def profile(username):
     return {f'This is a profile page for {username}'}
+
+#query parameter
+@app.get('/products')
+def products(id,price):
+    return {f'Product with an id: {id} and price = {price}'}
 
 
