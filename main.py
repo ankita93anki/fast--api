72feff552fda87fbf4ce1668012b4ed06b2396c4
ankita93anki <ankitasingh7639369@gmail.com>
2025-09-05 23:29:21 +0530

    Ordering of routes

--- main.py
+++ main.py
@@ -2,20 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get('/user/admin')
+def admin():
+    return {'This is admin page'}
 
-@app.get('/')
-def index():
-    return 'Hello world!'
+@app.get('/user/{username}')
+def profile(username):
+    return {f'This is a profile page for {username}'}
 
-#id here is a path parameter
-@app.get('/property/{id}')
-def property(id:int):
-    return {f'This is a property range for property {id}'}
 
-@app.get('/profile/{username}')
-def profile(username: str):
-    return {f'This is profile page for user : {username}'}
-
-@app.get('/movies')
-def movies():
-    return {'movie list': {'Movie 1','Movie 2'}}
